9c78e41c8f0dfa4c7ee12fbb2da2eacd473a3b34
piero7 <piero_chen@163.com>
2015-06-30 16:10:23 +0800

    finish file upload

---
+++ DemoForPark/Controllers/FileController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoForPark.Controllers
+{
+    /// <summary>
+    /// 文件上传
+    /// </summary>
+    public class FileController : Controller
+    {
+        //
+        // GET: /File/
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Upload(HttpPostedFileBase Filedata)
+        {
+            // 如果没有上传文件
+            if (Filedata == null ||
+                string.IsNullOrEmpty(Filedata.FileName) ||
+                Filedata.ContentLength == 0)
+            {
+                return this.HttpNotFound();
+            }
+
+            // 保存到 ~/photos 文件夹中，名称不变
+            string filename = System.IO.Path.GetFileName(Filedata.FileName);
+            string virtualPath =
+                string.Format("~/photos/{0}", filename);
+            // 文件系统不能使用虚拟路径
+            string path = this.Server.MapPath(virtualPath);
+
+            Filedata.SaveAs(path);
+            return this.Json(new { });
+        }
+    }
+}
